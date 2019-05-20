8d260b4d3b0cdbf3cea2874b25a9007304973853
Xiaohan Liu <747060970@qq.com>
2019-05-20 18:20:45 +1000

    did some port work

---
+++ SandSculpture/__main__.py
@@ -0,0 +1,2 @@
+def main():
+    player = ExamplePlayer("red")
--- SandSculpture/player.py
+++ SandSculpture/player.py
@@ -1,50 +1,157 @@
-
-class ExamplePlayer:
+class Player:
     def __init__(self, colour):
-        """
-        This method is called once at the beginning of the game to initialise
-        your player. You should use this opportunity to set up your own internal
-        representation of the game state, and any other information about the 
-        game state you would like to maintain for the duration of the game.
-
-        The parameter colour will be a string representing the player your 
-        program will play as (Red, Green or Blue). The value will be one of the 
-        strings "red", "green", or "blue" correspondingly.
-        """
-        # TODO: Set up state representation.
-
+        self.colour = colour
+        self.exited_count = 0
+        if colour == "red":
+            self.piece_loc = [(-3,0),(-3,1),(-3,2),(-3,3)]
+            self.dest_loc = [(3,-3),(3,-2),(3,-1),(3,0)]
+        if colour == "green":
+            self.piece_loc = [(0,-3),(1,-3),(2,-3),(3,-3)]
+            self.dest_loc = [(-3,3),(-2,3),(-1,3),(0,3)]
+        if colour == "blue":
+            self.piece_loc = [(3,0),(2,1),(1,2),(0,3)]
+            self.dest_loc = [(0,-3),(-1,-2),(-2,-1),(-3,0)]
 
     def action(self):
-        """
-        This method is called at the beginning of each of your turns to request 
-        a choice of action from your program.
-
-        Based on the current state of the game, your player should select and 
-        return an allowed action to play on this turn. If there are no allowed 
-        actions, your player must return a pass instead. The action (or pass) 
-        must be represented based on the above instructions for representing 
-        actions.
-        """
         # TODO: Decide what action to take.
-        return ("PASS", None)
 
+        return ("MOVE", ((-3,0), (-2,0)))
+        #return ("JUMP", ((-3,0), (-1,0)))
+        #return ("EXIT", (-3,0))
+        #return ("PASS", None)
 
     def update(self, colour, action):
+        if action[0]=="MOVE" or action[0]=="JUMP":
+            self.piece_loc[self.piece_loc.index(action[1][0])] = action[1][1]
+        if action[0]=="EXIT":
+            self.piece_loc.remove(action[1])
+        if action[0]=="PASS":
+            None
         """
-        This method is called at the end of every turn (including your player’s 
-        turns) to inform your player about the most recent action. You should 
-        use this opportunity to maintain your internal representation of the 
+        This method is called at the end of every turn (including your player’s
+        turns) to inform your player about the most recent action. You should
+        use this opportunity to maintain your internal representation of the
         game state and any other information about the game you are storing.
 
-        The parameter colour will be a string representing the player whose turn
-        it is (Red, Green or Blue). The value will be one of the strings "red", 
-        "green", or "blue" correspondingly.
-
-        The parameter action is a representation of the most recent action (or 
-        pass) conforming to the above in- structions for representing actions.
-
-        You may assume that action will always correspond to an allowed action 
-        (or pass) for the player colour (your method does not need to validate 
+        You may assume that action will always correspond to an allowed action
+        (or pass) for the player colour (your method does not need to validate
         the action/pass against the game rules).
         """
         # TODO: Update state representation in response to action.
+
+def main():
+    #initialise players
+    player1 = Player("red")
+    player2 = Player("green")
+    player3 = Player("blue")
+
+    #initialise board
+    board = {}
+    for tuple in player1.piece_loc:
+        board[tuple] = player1.colour
+    for tuple in player2.piece_loc:
+        board[tuple] = player2.colour
+    for tuple in player3.piece_loc:
+        board[tuple] = player3.colour
+    print_board(board)
+
+    #determine action for P1
+    player1_action = player1.action()
+
+    #update P1 state
+    player1.update(player1.colour,player1_action)
+
+    #update board
+    board = {}
+    for tuple in player1.piece_loc:
+        board[tuple] = player1.colour
+    for tuple in player2.piece_loc:
+        board[tuple] = player2.colour
+    for tuple in player3.piece_loc:
+        board[tuple] = player3.colour
+    print_board(board)
+
+
+def print_board(board_dict, message="", debug=False, **kwargs):
+    """
+    Helper function to print a drawing of a hexagonal board's contents.
+
+    Arguments:
+
+    * `board_dict` -- dictionary with tuples for keys and anything printable
+    for values. The tuple keys are interpreted as hexagonal coordinates (using
+    the axial coordinate system outlined in the project specification) and the
+    values are formatted as strings and placed in the drawing at the corres-
+    ponding location (only the first 5 characters of each string are used, to
+    keep the drawings small). Coordinates with missing values are left blank.
+
+    Keyword arguments:
+
+    * `message` -- an optional message to include on the first line of the
+    drawing (above the board) -- default `""` (resulting in a blank message).
+    * `debug` -- for a larger board drawing that includes the coordinates
+    inside each hex, set this to `True` -- default `False`.
+    * Or, any other keyword arguments! They will be forwarded to `print()`.
+    """
+
+    # Set up the board template:
+    if not debug:
+        # Use the normal board template (smaller, not showing coordinates)
+        template = """# {0}
+#           .-'-._.-'-._.-'-._.-'-.
+#          |{16:}|{23:}|{29:}|{34:}|
+#        .-'-._.-'-._.-'-._.-'-._.-'-.
+#       |{10:}|{17:}|{24:}|{30:}|{35:}|
+#     .-'-._.-'-._.-'-._.-'-._.-'-._.-'-.
+#    |{05:}|{11:}|{18:}|{25:}|{31:}|{36:}|
+#  .-'-._.-'-._.-'-._.-'-._.-'-._.-'-._.-'-.
+# |{01:}|{06:}|{12:}|{19:}|{26:}|{32:}|{37:}|
+# '-._.-'-._.-'-._.-'-._.-'-._.-'-._.-'-._.-'
+#    |{02:}|{07:}|{13:}|{20:}|{27:}|{33:}|
+#    '-._.-'-._.-'-._.-'-._.-'-._.-'-._.-'
+#       |{03:}|{08:}|{14:}|{21:}|{28:}|
+#       '-._.-'-._.-'-._.-'-._.-'-._.-'
+#          |{04:}|{09:}|{15:}|{22:}|
+#          '-._.-'-._.-'-._.-'-._.-'"""
+    else:
+        # Use the debug board template (larger, showing coordinates)
+        template = """# {0}
+#              ,-' `-._,-' `-._,-' `-._,-' `-.
+#             | {16:} | {23:} | {29:} | {34:} |
+#             |  0,-3 |  1,-3 |  2,-3 |  3,-3 |
+#          ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-.
+#         | {10:} | {17:} | {24:} | {30:} | {35:} |
+#         | -1,-2 |  0,-2 |  1,-2 |  2,-2 |  3,-2 |
+#      ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-.
+#     | {05:} | {11:} | {18:} | {25:} | {31:} | {36:} |
+#     | -2,-1 | -1,-1 |  0,-1 |  1,-1 |  2,-1 |  3,-1 |
+#  ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-.
+# | {01:} | {06:} | {12:} | {19:} | {26:} | {32:} | {37:} |
+# | -3, 0 | -2, 0 | -1, 0 |  0, 0 |  1, 0 |  2, 0 |  3, 0 |
+#  `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-'
+#     | {02:} | {07:} | {13:} | {20:} | {27:} | {33:} |
+#     | -3, 1 | -2, 1 | -1, 1 |  0, 1 |  1, 1 |  2, 1 |
+#      `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-'
+#         | {03:} | {08:} | {14:} | {21:} | {28:} |
+#         | -3, 2 | -2, 2 | -1, 2 |  0, 2 |  1, 2 | key:
+#          `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' ,-' `-.
+#             | {04:} | {09:} | {15:} | {22:} |   | input |
+#             | -3, 3 | -2, 3 | -1, 3 |  0, 3 |   |  q, r |
+#              `-._,-' `-._,-' `-._,-' `-._,-'     `-._,-'"""
+
+    # prepare the provided board contents as strings, formatted to size.
+    ran = range(-3, +3+1)
+    cells = []
+    for qr in [(q, r) for q in ran for r in ran if -q-r in ran]:
+        if qr in board_dict:
+            cell = str(board_dict[qr]).center(5)
+        else:
+            cell = "     "  # 5 spaces will fill a cell
+        cells.append(cell)
+
+    # fill in the template to create the board drawing, then print!
+    board = template.format(message, *cells)
+    print(board, **kwargs)
+
+if __name__ == '__main__':
+    main()
